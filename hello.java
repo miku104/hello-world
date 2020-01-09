5233a7f8a2833779dca4098567ca1eeb5787f3f7
miku104 <59693210+miku104@users.noreply.github.com>
2020-01-09 21:21:34 +0800

    delete file
    第一次使用github删除文件

--- hello.java
+++
@@ -1,9 +0,0 @@
-package cn.bdqn.test;
-
-public class Hello{
-  public static void main(String[] args){
-    System.out.println("hello world,github");
-    System.out.println("hello world,github");
-    System.out.println("hello world,github");
-  }
-}
